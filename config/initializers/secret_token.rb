eab82f17ddeade0a7fb23731f7c7defa546aac05
Adam Patterson <adam.patterson0012@gmail.com>
2014-07-24 21:33:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MagicDb::Application.config.secret_key_base = 'd259942523f13b5187b6980c95842372050a0014d1c32a7a6b5d17797554d761550ff65ca6e21400d38972af932d367f2c50bb0c08265c9e874ad30b163b02ed'
