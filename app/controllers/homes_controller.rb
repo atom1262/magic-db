a155ae396a5beec21603e551d95d482eb7bf9436
Adam Patterson <adam.patterson0012@gmail.com>
2014-07-26 14:45:00 -0400

    orders collections on homes#show by recently created_at

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,8 @@
 class HomesController < ApplicationController
 
   def show
-    @tricks = Trick.all
-    @posts = Post.all
+    @tricks = Trick.all.order("created_at DESC")
+    @posts = Post.all.order("created_at DESC")
   end
 
 end
